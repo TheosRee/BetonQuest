3f1ff63f97bd277ee44d80764d10406ac632de1f
TheosRee <theosree@users.noreply.github.com>
2024-05-03 22:55:48 +0200

    convert Conditions

---
+++ src/main/java/org/betonquest/betonquest/compatibility/npcs/abstractnpc/conditions/location/NPCLocationConditionFactory.java
@@ -0,0 +1,32 @@
+package org.betonquest.betonquest.compatibility.npcs.abstractnpc.conditions.location;
+
+import org.betonquest.betonquest.Instruction;
+import org.betonquest.betonquest.api.quest.condition.Condition;
+import org.betonquest.betonquest.api.quest.condition.ConditionFactory;
+import org.betonquest.betonquest.compatibility.npcs.abstractnpc.BQNPCAdapter;
+import org.betonquest.betonquest.compatibility.npcs.abstractnpc.NPCSupplierStandard;
+import org.betonquest.betonquest.exceptions.InstructionParseException;
+import org.betonquest.betonquest.instruction.variable.VariableNumber;
+import org.betonquest.betonquest.instruction.variable.location.VariableLocation;
+
+import java.util.function.Supplier;
+
+/**
+ * Factory to create {@link NPCLocationCondition}s from {@link Instruction}s.
+ */
+public abstract class NPCLocationConditionFactory implements ConditionFactory, NPCSupplierStandard {
+    /**
+     * The default Constructor.
+     */
+    public NPCLocationConditionFactory() {
+    }
+
+    @Override
+    public Condition parse(final Instruction instruction) throws InstructionParseException {
+        final String npcId = instruction.next();
+        final Supplier<BQNPCAdapter> supplier = getSupplierByID(npcId);
+        final VariableLocation location = instruction.getLocation();
+        final VariableNumber radius = instruction.getVarNum();
+        return new NPCLocationCondition(npcId, supplier, location, radius);
+    }
+}
