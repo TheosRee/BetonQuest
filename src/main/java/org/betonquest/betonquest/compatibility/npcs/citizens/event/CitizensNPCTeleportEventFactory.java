f11cc2b4210a180bcabb65ded9609dae80d8c939
TheosRee <theosree@users.noreply.github.com>
2024-05-04 21:06:29 +0200

    extract NPCTeleportEvent add NPCUtil for central error message place

---
+++ src/main/java/org/betonquest/betonquest/compatibility/npcs/citizens/event/CitizensNPCTeleportEventFactory.java
@@ -0,0 +1,33 @@
+package org.betonquest.betonquest.compatibility.npcs.citizens.event;
+
+import org.betonquest.betonquest.Instruction;
+import org.betonquest.betonquest.api.quest.event.ComposedEvent;
+import org.betonquest.betonquest.compatibility.npcs.abstractnpc.event.teleport.NPCTeleportEventFactory;
+import org.betonquest.betonquest.compatibility.npcs.citizens.CitizensNPCSupplier;
+import org.betonquest.betonquest.exceptions.InstructionParseException;
+import org.betonquest.betonquest.quest.PrimaryServerThreadData;
+import org.betonquest.betonquest.quest.event.PrimaryServerThreadComposedEvent;
+
+/**
+ * Citizens implementation for {@link NPCTeleportEventFactory}.
+ */
+public class CitizensNPCTeleportEventFactory extends NPCTeleportEventFactory implements CitizensNPCSupplier {
+    /**
+     * Data to use for syncing to the primary server thread.
+     */
+    private final PrimaryServerThreadData data;
+
+    /**
+     * Create a new factory for Citizens teleport events.
+     *
+     * @param data the data to use for syncing to the primary server thread
+     */
+    public CitizensNPCTeleportEventFactory(final PrimaryServerThreadData data) {
+        this.data = data;
+    }
+
+    @Override
+    public ComposedEvent parseComposedEvent(final Instruction instruction) throws InstructionParseException {
+        return new PrimaryServerThreadComposedEvent(super.parseComposedEvent(instruction), data);
+    }
+}
