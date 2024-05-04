a0202fdb90e12fd7d654cc947ebd5db00b2b9d8b
TheosRee <theosree@users.noreply.github.com>
2024-05-04 21:06:29 +0200

    extract NPCTeleportEvent and migrate to new event API add NPCUtil for central error message place

--- src/main/java/org/betonquest/betonquest/compatibility/npcs/citizens/CitizensIntegrator.java
+++ src/main/java/org/betonquest/betonquest/compatibility/npcs/citizens/CitizensIntegrator.java
@@ -11,6 +11,7 @@ import org.betonquest.betonquest.compatibility.npcs.citizens.events.move.Citizen
 import org.betonquest.betonquest.compatibility.npcs.citizens.events.move.CitizensMoveEvent;
 import org.betonquest.betonquest.compatibility.npcs.citizens.events.move.CitizensMoveEventFactory;
 import org.betonquest.betonquest.compatibility.npcs.citizens.events.move.CitizensStopEventFactory;
+import org.betonquest.betonquest.compatibility.npcs.citizens.events.teleport.CitizensNPCTeleportEventFactory;
 import org.betonquest.betonquest.compatibility.npcs.citizens.variables.CitizensVariableFactory;
 import org.betonquest.betonquest.compatibility.protocollib.hider.NPCHider;
 import org.betonquest.betonquest.compatibility.protocollib.hider.UpdateVisibilityNowEvent;
@@ -77,7 +78,7 @@ public class CitizensIntegrator implements Integrator {
         final EventTypeRegistry eventTypes = questRegistries.getEventTypes();
         eventTypes.register("movenpc", new CitizensMoveEventFactory(primaryServerThreadData, citizensMoveController));
         eventTypes.register("stopnpc", new CitizensStopEventFactory(primaryServerThreadData, citizensMoveController));
-        plugin.registerEvents("teleportnpc", NPCTeleportEvent.class);
+        eventTypes.register("teleportnpc", new CitizensNPCTeleportEventFactory(primaryServerThreadData));
         plugin.registerConversationIO("chest", CitizensInventoryConvIO.class);
         plugin.registerConversationIO("combined", CitizensInventoryConvIO.CitizensCombined.class);
         questRegistries.getVariableTypes().register("citizen", new CitizensVariableFactory(loggerFactory));
