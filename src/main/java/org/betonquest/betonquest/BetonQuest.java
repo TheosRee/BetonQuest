b731352cdf7facd9d7eeb2af13661a623140dcf2
TheosRee <theosree@users.noreply.github.com>
2025-03-06 18:47:09 +0100

    crush QuestItem into Interface and Processor

--- src/main/java/org/betonquest/betonquest/BetonQuest.java
+++ src/main/java/org/betonquest/betonquest/BetonQuest.java
@@ -414,7 +414,8 @@ public class BetonQuest extends JavaPlugin {
 
         setupUpdater();
 
-        rpgMenu = new RPGMenu(loggerFactory.create(RPGMenu.class), loggerFactory, config, pluginMessage, questTypeAPI, profileProvider);
+        rpgMenu = new RPGMenu(loggerFactory.create(RPGMenu.class), loggerFactory, config, pluginMessage,
+                questTypeAPI, featureAPI, profileProvider);
 
         log.info("BetonQuest successfully enabled!");
     }
@@ -451,7 +452,7 @@ public class BetonQuest extends JavaPlugin {
         List.of(
                 new CombatTagger(profileProvider, config.getInt("combat_delay")),
                 new MobKillListener(profileProvider),
-                new CustomDropListener(loggerFactory.create(CustomDropListener.class), this),
+                new CustomDropListener(loggerFactory.create(CustomDropListener.class), this, featureAPI),
                 new QuestItemHandler(config, playerDataStorage, pluginMessage, profileProvider),
                 new JoinQuitListener(loggerFactory, coreQuestRegistry.objectives(), playerDataStorage,
                         pluginMessage, profileProvider)
